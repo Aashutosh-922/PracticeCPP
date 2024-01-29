3e555dae64527bd1cc2ac6526b65defb06e2c0a4
Aashutosh Karale <80333681+Aashutosh-922@users.noreply.github.com>
2024-01-30 01:36:42 +0530

    Create dp_fibonnaci.cpp
    cxc

---
+++ dp_fibonnaci.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n, vector<int>& dp){
+    if(n<=1) return n;
+    
+    if(dp[n]!= -1) return dp[n];
+    return dp[n]= f(n-1,dp) + f(n-2,dp);
+}
+
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  cout<<f(n,dp);
+  return 0;
+}
